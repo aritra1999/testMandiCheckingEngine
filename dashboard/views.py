ba16847d516311fda945071f341ea645fd829a93
Aritra Mondal <mondalaritra002@gmail.com>
2020-06-16 08:42:00 +0530

    Deploying to Heroku.

--- dashboard/views.py
+++ dashboard/views.py
@@ -44,8 +44,12 @@ def question_details_view(request, hit):
             Submission.objects.create(user=user, question_hit=question_hit, time_taken=time_taken, verdict=verdict, solution=solution, language=language).save()
 
     code = Submission.objects.filter(user=user, question_hit=hit).order_by('-time_stamp')
-    context["code"] = code.first().solution
-    context["language"] = code.first().language
+    if code.count > 0:
+        context["code"] = code.first().solution
+        context["language"] = code.first().language
+    else:
+        context["code"] = ""
+        context["language"] = "c_cpp"
 
     return render(request, 'dashboard/question.html', context)
 
