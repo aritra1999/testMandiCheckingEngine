d9d2a0459c4e9ea4e659cb9c3f19913cbe88dc1d
Aritra Mondal <mondalaritra002@gmail.com>
2020-07-23 01:19:49 +0530

    Added OTP Email Auth

--- Testmandi/settings.py
+++ Testmandi/settings.py
@@ -142,6 +142,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mondalaritra002@gmail.com'
-EMAIL_HOST_PASSWORD = 'captaindeadpool'
+EMAIL_HOST_USER = 'exmail'
+EMAIL_HOST_PASSWORD = 'password'
 
